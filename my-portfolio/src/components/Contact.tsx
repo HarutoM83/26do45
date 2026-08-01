1a8f8ca5c06382078e9c12f4a5936e988e70601a
HarutoM83 <ktc24a32a0010@edu.kyoto-tech.ac.jp>
2026-08-01 15:27:52 +0900

    エラー全部消えた

--- my-portfolio/src/components/Contact.tsx
+++ my-portfolio/src/components/Contact.tsx
@@ -7,7 +7,7 @@ export default function Contact(){
 
     const links:{ label: string;icon:ReactNode; href: string|null}[] = [
         {label:'Github',icon:<FaGithub/>,href:profile.links.github},
-        {label:'X（旧Twitter）',icon:<FaXTwitter/>,href:profile.links.github},
+        {label:'X（旧Twitter）',icon:<FaXTwitter/>,href:profile.links.twitter},
         {label:'Email',icon:<FaEnvelope/>,href:profile.links.email},
     ]
 
