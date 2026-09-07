75de7b1c139442150fc421064da5a271ef022728
HarutoM83 <ktc24a32a0010@edu.kyoto-tech.ac.jp>
2026-09-07 15:27:47 +0900

    Revert "ジャンル指定"
    This reverts commit 6b8ccd3e74019d96d6b5143ad7eaea6cba9af88d.

--- my-portfolio/src/data/portfolio.ts
+++ my-portfolio/src/data/portfolio.ts
@@ -178,7 +178,7 @@ export const works: Work[]=[
     {
         id: 3,
         title: "メカドッジリミットブレイク",
-        genre: "3Dアクション",
+        genre: "3D",
         category: "game",
         thumbnail: "",
         screenshots: [],
